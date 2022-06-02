39195c2768ae02e0cdd4367c16b0c23cbd2fee41
Karimof <islamkarimov8580@gmail.com>
2022-06-02 15:04:36 +0500

    finished

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -17,11 +17,6 @@ public class StudentController {
     //1. VAZIRLIK
     @GetMapping("/forMinistry")
     public Page<Student> getStudentListForMinistry(@RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
         Pageable pageable = PageRequest.of(page, 10);
         Page<Student> studentPage = studentRepository.findAll(pageable);
         return studentPage;
@@ -31,18 +26,26 @@ public class StudentController {
     @GetMapping("/forUniversity/{universityId}")
     public Page<Student> getStudentListForUniversity(@PathVariable Integer universityId,
                                                      @RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
+
         Pageable pageable = PageRequest.of(page, 10);
         Page<Student> studentPage = studentRepository.findAllByGroup_Faculty_UniversityId(universityId, pageable);
         return studentPage;
     }
 
     //3. FACULTY DEKANAT
-    //4. GROUP OWNER
+    @GetMapping("/facultyDekanat/{facultyId}")
+    public Page<Student> som(@PathVariable Integer facultyId, @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return studentRepository.findAllByGroup_Faculty_Id(facultyId, pageable);
+    }
 
+    //4. GROUP OWNER
+    @GetMapping("/groupOwner/{groupId}")
+    public Page<Student> findAllByGroup_Id(@PathVariable Integer groupId, @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Student> allByGroup_id = studentRepository.findAllByGroup_Id(groupId, pageable);
+        return allByGroup_id;
+
+    }
 
 }
